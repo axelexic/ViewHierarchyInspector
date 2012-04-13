50e6e17f6f71fd09abba0a16299b107218f780d1
Yogesh Swami <axelexic@gmail.com>
2012-04-12 22:41:41 -0700

    Initial Commit

---
+++ ViewHierarchyInspector/ViewHierarchyInspector.h
@@ -0,0 +1,13 @@
+//
+//  ViewHierarchyInspector.h
+//  ViewHierarchyInspector
+//
+//  Created by Yogesh Swami on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ViewHierarchyInspector : NSObject
+
+@end
---
+++ ViewHierarchyInspector/ViewHierarchyInspector.m
@@ -0,0 +1,13 @@
+//
+//  ViewHierarchyInspector.m
+//  ViewHierarchyInspector
+//
+//  Created by Yogesh Swami on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewHierarchyInspector.h"
+
+@implementation ViewHierarchyInspector
+
+@end
